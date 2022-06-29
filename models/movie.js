08b017a16e6646a610c9731b15bd4e297c9f8bda
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-06-30 00:39:01 +0500

    Make schemes and modeles

---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: [{
+      validator: (link) => validator.isURL(link),
+      message: 'Пожалуйста, введите корректную ссылку на изображение',
+    },
+    ],
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: [{
+      validator: (link) => validator.isURL(link),
+      message: 'Пожалуйста, введите корректную ссылку на изображение',
+    },
+    ],
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: [{
+      validator: (link) => validator.isURL(link),
+      message: 'Пожалуйста, введите корректную ссылку на изображение',
+    },
+    ],
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
