084f4902c26b38f9ee48361d7901a99025c1e61a
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-07-02 19:46:42 +0500

    add joi validation to movie post route

--- routes/movies.js
+++ routes/movies.js
@@ -2,17 +2,41 @@ const router = require('express').Router();
 const {
   celebrate, Joi,
 } = require('celebrate');
+const validator = require('validator');
 const {
   getMovies,
   createMovie,
   deleteMovieById,
 } = require('../controllers/movies');
+const BadRequestError = require('../errors/badRequest');
+
+const validateURL = (value) => {
+  if (!validator.isURL(value, { require_protocol: true })) {
+    throw new BadRequestError('Неправильный формат ссылки');
+  }
+  return value;
+};
 
 router.get('/', getMovies);
-router.post('/', createMovie);
+router.post('/', celebrate({
+  body: {
+    country: Joi.string().min(1).required(),
+    director: Joi.string().min(1).required(),
+    duration: Joi.number().integer().required(),
+    description: Joi.string().min(1).required(),
+    image: Joi.string().custom(validateURL).required(),
+    movieId: Joi.string().length(24).hex().required(),
+    nameRU: Joi.string().min(1).required(),
+    nameEN: Joi.string().min(1).required(),
+    owner: Joi.string().length(24).hex().required(),
+    thumbnail: Joi.string().custom(validateURL).required(),
+    trailerLink: Joi.string().custom(validateURL).required(),
+    year: Joi.string().max(4).required(),
+  },
+}), createMovie);
 router.delete('/:movieId', celebrate({
   params: {
-    cardId: Joi.string().length(24).hex().required(),
+    movieId: Joi.string().length(24).hex().required(),
   },
 }), deleteMovieById);
 
