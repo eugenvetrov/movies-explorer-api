a431be3a6febcfff4ae1a5fe63afae67016b3b1a
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-07-02 20:59:27 +0500

    change db's name

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const NotFoundError = require('./errors/notFound');
 const { PORT = 5000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/moviedb');
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(cors());
 app.use(cookieParser());
